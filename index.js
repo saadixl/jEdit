4d2945f407a2a9325b7bc3ca24e714228e9c8dbb
saadixl <saadixl@outlook.com>
2015-12-05 17:49:51 +0600

    so far its working

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ var app = express();
 app.use(express.static(__dirname));
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-app.listen(7070);
+server.listen(7070);
+var receivedJSCode = "";
 
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'this is a message from server' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
+io.sockets.on('connection', function(socket) {
+    socket.on('send', function(data) {
+        
+        var receivedJSCode = data.message;
+        var modifiedReceivedJSCode = receivedJSCode.replace("'", "\'");
+        var theJavaScriptSourceCode = 'var myInjectedVariable = "";' + modifiedReceivedJSCode + 'function print(p){myInjectedVariable+=p;myInjectedVariable+="\\n";}exports.theFinalReturnedThing = myInjectedVariable;';
+
+        var res = _eval(theJavaScriptSourceCode);
+        console.log(res.theFinalReturnedThing);
+
+        io.sockets.emit('message', {
+            message: res.theFinalReturnedThing
+        });
+
     });
 });
-
-var receivedJSCode = 'var x = 5; print(x); print(x++);';
-var modifiedReceivedJSCode = receivedJSCode.replace("'", "\'");
-var theJavaScriptSourceCode = 'var myInjectedVariable = "";' + modifiedReceivedJSCode + 'function print(p){myInjectedVariable+=p;myInjectedVariable+="\\n";}exports.theFinalReturnedThing = myInjectedVariable;';
-
-
-var res = _eval(theJavaScriptSourceCode);
-console.log(res.theFinalReturnedThing);
--- js/jedit.js
+++ js/jedit.js
@@ -2,16 +2,16 @@ var jsEditor = ace.edit("jsEditor");
 jsEditor.setTheme("ace/theme/monokai");
 jsEditor.getSession().setMode("ace/mode/javascript");
 
-$("#jsEditor").keyup(function() {
-    $('#result').text(jsEditor.getValue());
+jsEditor.setValue("/* use print() instead of using console.log() */")
+
+$(".runButton").click(function() {
+    socket.emit('send', {
+        message: jsEditor.getValue()
+    });
 });
 
-var socket = io.connect('/');
+var socket = io.connect('//localhost:7070/');
 
-socket.emit('send', { message: 'yo yo yo from browser' });
-
-socket.on('message', function (data) {
-       console.log(data.message);
+socket.on('message', function(data) {
+    $('#result').text(data.message);
 });
-
-
