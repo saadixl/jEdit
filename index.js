77bc31e3004f2e4ed4c0d50136050fa5610c35b1
saadixl <saadixl@outlook.com>
2015-12-05 16:45:20 +0600

    socket io omg :P

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ var express = require('express');
 var _eval = require('eval');
 var app = express();
 app.use(express.static(__dirname));
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 app.listen(7070);
-var receivedJSCode = 'var x = 5; console.log(x); console.log(x++);';
-var modifiedReceivedJSCode = receivedJSCode.replace("console.log", "myInjectedVariable +=");
-var theJavaScriptSourceCode = 'var myInjectedVariable = "";' + modifiedReceivedJSCode +'exports.theFinalReturnedThing = myInjectedVariable;';
 
-//console.log(x);
-//myInjectedVariable +=
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'this is a message from server' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+var receivedJSCode = 'var x = 5; print(x); print(x++);';
+var modifiedReceivedJSCode = receivedJSCode.replace("'", "\'");
+var theJavaScriptSourceCode = 'var myInjectedVariable = "";' + modifiedReceivedJSCode + 'function print(p){myInjectedVariable+=p;myInjectedVariable+="\\n";}exports.theFinalReturnedThing = myInjectedVariable;';
+
 
 var res = _eval(theJavaScriptSourceCode);
 console.log(res.theFinalReturnedThing);
--- js/jedit.js
+++ js/jedit.js
@@ -2,6 +2,16 @@ var jsEditor = ace.edit("jsEditor");
 jsEditor.setTheme("ace/theme/monokai");
 jsEditor.getSession().setMode("ace/mode/javascript");
 
-$("#jsEditor").keyup(function(){
-	$('#result').text(jsEditor.getValue());
+$("#jsEditor").keyup(function() {
+    $('#result').text(jsEditor.getValue());
 });
+
+var socket = io.connect('/');
+
+socket.emit('send', { message: 'yo yo yo from browser' });
+
+socket.on('message', function (data) {
+       console.log(data.message);
+});
+
+
